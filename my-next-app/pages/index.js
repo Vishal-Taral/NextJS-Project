ea6073510b97ece98199c87cee7d58c875421fdb
vishal taral <vishaltaral09@gmail.com>
2023-01-21 16:37:24 +0530

    css changed

--- my-next-app/pages/index.js
+++ my-next-app/pages/index.js
@@ -1,11 +1,20 @@
 import Layout from "@/Components/Layout";
 import { useRouter } from "next/router";
+import Image from "next/image";
+import HomePageImage from '/public/HomePage.jpg';
 
 export default function Home() {
   const router = useRouter();
   return (
     <Layout>
-      <h1 className="text-center" onClick={() => {router.push('/users')}} style={{cursor:"pointer"}}>first next js project</h1>
+      <div style={{ textAlign: "center" }}>
+        <h1 className="text-center" onClick={() => { router.push('/users') }} style={{ cursor: "pointer" }}>first next js project</h1>
+        <p>please click on the obove text to see employee's list </p>
+      </div>
+
+      <div className="centeredItem">
+        <Image src={HomePageImage} alt="Home page image" style={{ marginTop: "2%", marginBottom: "2%", borderRadius: "10px" }}></Image>
+      </div>
     </Layout>
   )
 }
--- my-next-app/pages/users/index.js
+++ my-next-app/pages/users/index.js
@@ -12,23 +12,23 @@ const Index = ({ users }) => {
             <div className='container'>
                 <div className={styles.main_Header}>
                     <h1><u>All Users</u> </h1>
-                    <button className='rounded bg-secondary text-light text-uppercase' onClick={() => {router.push('/')}}>go to Home page</button>
-                </div> 
+                    <button className='rounded bg-secondary text-light text-uppercase' onClick={() => { router.push('/') }}>go to Home page</button>
+                </div>
 
-                <table className='table'>
+                <table className='table table-striped table-dark mt-2 rounded'>
                     <thead>
-                        <tr> 
-                            <th className='heading'>Name</th>
+                        <tr>
                             <th className='heading'>User ID</th>
-                            <th className='heading'>Company Detail</th>
+                            <th className='heading'>User's Name</th>
+                            <th className='heading'>User's Company Detail</th>
                         </tr>
                     </thead>
                     <tbody>
                         {users.map(user => (
                             <tr key={user.id}>
-                                <td>{user.id}</td>
-                                <td onClick={() => { router.push(`/users/${user.id}`) }} style={{ cursor: "pointer" }}>{user.name}</td>
-                                <td><b>Comany Name :-</b>{` ${user.company.name}`} <br /> <b>catchPhrase :-</b> {`  ${user.company.catchPhrase}`}</td>
+                                <td onClick={() => { router.push(`/users/${user.id}`) }} style={{ cursor: "pointer" }}>{user.id}</td>
+                                <td onClick={() => { router.push(`/users/${user.id}`) }} style={{ cursor: "pointer" }}><u>{user.name}</u></td>
+                                <td onClick={() => { router.push(`/users/${user.id}`) }} style={{ cursor: "pointer" }}><b>Comany Name :-</b>{` ${user.company.name}`} <br /> <b>catchPhrase :-</b> {`  ${user.company.catchPhrase}`}</td>
                             </tr>
                         ))}
                     </tbody>
@@ -51,7 +51,7 @@ const Index = ({ users }) => {
 export default Index;
 
 export async function getStaticProps() {
-    
+
     try {
         const { data } = await axios.get('https://jsonplaceholder.typicode.com/users')
         return {
